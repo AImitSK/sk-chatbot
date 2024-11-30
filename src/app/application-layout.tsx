e22571450c1705bf39da2f80cb8b2f02b72a8a84
Stefan Kuehne <s.kuehne@sk-online-marketing.de>
2024-11-30 09:22:03 +0100

    Initialer Commit

---
+++ src/app/application-layout.tsx
@@ -0,0 +1,144 @@
+'use client'
+
+import { Avatar } from '@/components/avatar'
+import {
+  Dropdown,
+  DropdownButton,
+  DropdownDivider,
+  DropdownItem,
+  DropdownLabel,
+  DropdownMenu,
+} from '@/components/dropdown'
+import { Navbar, NavbarItem, NavbarSection, NavbarSpacer } from '@/components/navbar'
+import {
+  Sidebar,
+  SidebarBody,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarHeading,
+  SidebarItem,
+  SidebarLabel,
+  SidebarSection,
+  SidebarSpacer,
+} from '@/components/sidebar'
+import { SidebarLayout } from '@/components/sidebar-layout'
+import { getEvents } from '@/data'
+import {
+  ArrowRightStartOnRectangleIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+  Cog8ToothIcon,
+  LightBulbIcon,
+  PlusIcon,
+  ShieldCheckIcon,
+  UserCircleIcon,
+} from '@heroicons/react/16/solid'
+import {
+  Cog6ToothIcon,
+  HomeIcon,
+  QuestionMarkCircleIcon,
+  SparklesIcon,
+  Square2StackIcon,
+  TicketIcon,
+} from '@heroicons/react/20/solid'
+import { usePathname } from 'next/navigation'
+import Image from 'next/image'
+
+function AccountDropdownMenu({ anchor }: { anchor: 'top start' | 'bottom end' }) {
+  return (
+    <DropdownMenu className="min-w-64" anchor={anchor}>
+      <DropdownItem href="#">
+        <ArrowRightStartOnRectangleIcon />
+        <DropdownLabel>Sign out</DropdownLabel>
+      </DropdownItem>
+    </DropdownMenu>
+  )
+}
+
+export function ApplicationLayout({
+  events,
+  children,
+}: {
+  events: Awaited<ReturnType<typeof getEvents>>
+  children: React.ReactNode
+}) {
+  let pathname = usePathname()
+
+  return (
+    <SidebarLayout
+      navbar={
+        <Navbar>
+          <NavbarSpacer />
+          <NavbarSection>
+            <Dropdown>
+              <DropdownButton as={NavbarItem}>
+                <Avatar src="/users/erica.jpg" square />
+              </DropdownButton>
+              <AccountDropdownMenu anchor="bottom end" />
+            </Dropdown>
+          </NavbarSection>
+        </Navbar>
+      }
+      sidebar={
+        <Sidebar>
+          <SidebarHeader>
+            <Image
+              src="/logo-sk-online-marketing.svg"
+              alt="Logo"
+              width={50}
+              height={50}
+              style={{ width: '120px', height: '50px' }}
+            />
+          </SidebarHeader>
+
+          <SidebarBody>
+            <SidebarSection>
+              <SidebarItem href="/" current={pathname === '/'}>
+                <HomeIcon />
+                <SidebarLabel>Dashboard</SidebarLabel>
+              </SidebarItem>
+              <SidebarItem href="/profil" current={pathname.startsWith('/profil')}>
+                <Cog6ToothIcon />
+                <SidebarLabel>Profile</SidebarLabel>
+              </SidebarItem>
+              <SidebarItem href="/orders" current={pathname.startsWith('/orders')}>
+                <TicketIcon />
+                <SidebarLabel>Rechnungen</SidebarLabel>
+              </SidebarItem>
+            </SidebarSection>
+
+
+            <SidebarSpacer />
+
+            <SidebarSection>
+              <SidebarItem href="/support">
+                <QuestionMarkCircleIcon />
+                <SidebarLabel>Support</SidebarLabel>
+              </SidebarItem>
+            </SidebarSection>
+          </SidebarBody>
+
+          <SidebarFooter className="max-lg:hidden">
+            <Dropdown>
+              <DropdownButton as={SidebarItem}>
+                <span className="flex min-w-0 items-center gap-3">
+                  <Avatar src="/users/erica.jpg" className="size-10" square alt="" />
+                  <span className="min-w-0">
+                    <span className="block truncate text-sm/5 font-medium text-zinc-950 dark:text-white">Erica</span>
+                    <span className="block truncate text-xs/5 font-normal text-zinc-500 dark:text-zinc-400">
+                      erica@example.com
+                    </span>
+                  </span>
+                </span>
+                <ChevronUpIcon />
+              </DropdownButton>
+              <AccountDropdownMenu anchor="top start" />
+            </Dropdown>
+          </SidebarFooter>
+        </Sidebar>
+      }
+    >
+      {children}
+    </SidebarLayout>
+  )
+}
