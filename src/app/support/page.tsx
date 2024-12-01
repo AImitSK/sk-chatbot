648ad5ee73984ade6c60571a318fb07ee2eae38f
Stefan Kuehne <s.kuehne@sk-online-marketing.de>
2024-12-01 17:02:54 +0100

    Refactor codebase and integrate Sanity client.
    This commit removes redundant .gitignore files and settings while introducing Sanity client setup for fetching and displaying post data. It also updates the package-lock.json to include necessary dependencies for Sanity integration and modifies the support page to utilize this data.

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -1,96 +1,31 @@
-import { Button } from '@/components/button'
-import { Checkbox, CheckboxField } from '@/components/checkbox'
-import { Divider } from '@/components/divider'
-import { Label } from '@/components/fieldset'
-import { Heading, Subheading } from '@/components/heading'
-import { Input } from '@/components/input'
-import { Select } from '@/components/select'
-import { Text } from '@/components/text'
-import { Textarea } from '@/components/textarea'
-import type { Metadata } from 'next'
-import { Address } from './address'
 
-export const metadata: Metadata = {
-  title: 'Settings',
-}
+import Link from "next/link";
+import { type SanityDocument } from "next-sanity";
+import { client } from "@/sanity/client";
 
-export default function Settings() {
-  return (
-    <form method="post" className="mx-auto max-w-4xl">
-      <Heading>Settings</Heading>
-      <Divider className="my-10 mt-6" />
+const POSTS_QUERY = `*[
+  _type == "post"
+  && defined(slug.current)
+]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt}`;
 
-      <section className="grid gap-x-8 gap-y-6 sm:grid-cols-2">
-        <div className="space-y-1">
-          <Subheading>Organization Name</Subheading>
-          <Text>This will be displayed on your public profile.</Text>
-        </div>
-        <div>
-          <Input aria-label="Organization Name" name="name" defaultValue="Catalyst" />
-        </div>
-      </section>
+const options = { next: { revalidate: 30 } };
 
-      <Divider className="my-10" soft />
+export default async function IndexPage() {
+    const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
 
-      <section className="grid gap-x-8 gap-y-6 sm:grid-cols-2">
-        <div className="space-y-1">
-          <Subheading>Organization Bio</Subheading>
-          <Text>This will be displayed on your public profile. Maximum 240 characters.</Text>
-        </div>
-        <div>
-          <Textarea aria-label="Organization Bio" name="bio" />
-        </div>
-      </section>
-
-      <Divider className="my-10" soft />
-
-      <section className="grid gap-x-8 gap-y-6 sm:grid-cols-2">
-        <div className="space-y-1">
-          <Subheading>Organization Email</Subheading>
-          <Text>This is how customers can contact you for support.</Text>
-        </div>
-        <div className="space-y-4">
-          <Input type="email" aria-label="Organization Email" name="email" defaultValue="info@example.com" />
-          <CheckboxField>
-            <Checkbox name="email_is_public" defaultChecked />
-            <Label>Show email on public profile</Label>
-          </CheckboxField>
-        </div>
-      </section>
-
-      <Divider className="my-10" soft />
-
-      <section className="grid gap-x-8 gap-y-6 sm:grid-cols-2">
-        <div className="space-y-1">
-          <Subheading>Address</Subheading>
-          <Text>This is where your organization is registered.</Text>
-        </div>
-        <Address />
-      </section>
-
-      <Divider className="my-10" soft />
-
-      <section className="grid gap-x-8 gap-y-6 sm:grid-cols-2">
-        <div className="space-y-1">
-          <Subheading>Currency</Subheading>
-          <Text>The currency that your organization will be collecting.</Text>
-        </div>
-        <div>
-          <Select aria-label="Currency" name="currency" defaultValue="cad">
-            <option value="cad">CAD - Canadian Dollar</option>
-            <option value="usd">USD - United States Dollar</option>
-          </Select>
-        </div>
-      </section>
-
-      <Divider className="my-10" soft />
-
-      <div className="flex justify-end gap-4">
-        <Button type="reset" plain>
-          Reset
-        </Button>
-        <Button type="submit">Save changes</Button>
-      </div>
-    </form>
-  )
+    return (
+        <main className="container mx-auto min-h-screen max-w-3xl p-8">
+            <h1 className="text-4xl font-bold mb-8">Posts</h1>
+            <ul className="flex flex-col gap-y-4">
+                {posts.map((post) => (
+                    <li className="hover:underline" key={post._id}>
+                        <Link href={`/${post.slug.current}`}>
+                            <h2 className="text-xl font-semibold">{post.title}</h2>
+                            <p>{new Date(post.publishedAt).toLocaleDateString()}</p>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </main>
+    );
 }
